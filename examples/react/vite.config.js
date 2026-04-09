06fc994dd9df5203cbd8d172e4069bfa62672f06
Nico Martin <mail@nico.dev>
2026-04-09 06:25:04 +0200

    prettier to devDependencies

--- examples/react/vite.config.js
+++ examples/react/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        fs: {
-            allow: ["../.."],
-        },
+  plugins: [react()],
+  server: {
+    fs: {
+      allow: ["../.."],
     },
+  },
 });
